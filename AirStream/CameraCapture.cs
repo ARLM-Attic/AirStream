ab95d617d04d9b9518e2c9c0daf6ee49479cf132
CJ van der Smissen <vandersmissenc@gmail.com>
2014-04-15 07:34:07 -0500

    Initial Commit
    Initial Commit

---
+++ AirStream/CameraCapture.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Devices.Enumeration;
+using Windows.Media.Capture;
+using Windows.Media.Devices;
+using Windows.Media.MediaProperties;
+using Windows.Storage;
+
+namespace AirStream
+{
+    class CameraCapture : IDisposable
+    {
+        MediaCapture mediaCapture;
+        ImageEncodingProperties imgEncodingProperties;
+        MediaEncodingProfile videoEncodingProperties;
+
+        public VideoDeviceController VideoDeviceController
+        {
+            get { return mediaCapture.VideoDeviceController; }
+        }
+        public async Task<MediaCapture> Initialize(CaptureUse primaryUse = CaptureUse.Photo)
+        {
+            var devices = await DeviceInformation.FindAllAsync(DeviceClass.VideoCapture);
+            mediaCapture = new MediaCapture();
+            if (devices.Count() > 0)
+            {
+                await this.mediaCapture.InitializeAsync(new MediaCaptureInitializationSettings { VideoDeviceId = devices.ElementAt(1).Id, PhotoCaptureSource = Windows.Media.Capture.PhotoCaptureSource.VideoPreview });
+            }  
+            mediaCapture.VideoDeviceController.PrimaryUse = primaryUse;
+            mediaCapture.SetPreviewRotation(VideoRotation.Clockwise90Degrees);
+
+            imgEncodingProperties = ImageEncodingProperties.CreateJpeg();
+            imgEncodingProperties.Width = 640;
+            imgEncodingProperties.Height = 480;
+
+            videoEncodingProperties = MediaEncodingProfile.CreateMp4(VideoEncodingQuality.Auto); 
+
+            return mediaCapture;
+        }
+
+        public async Task StartPreview()
+        {
+            await mediaCapture.StartPreviewAsync();
+        }
+
+        public async Task StopPreview()
+        {
+            await mediaCapture.StopPreviewAsync();
+        }
+
+        public async Task<StorageFile> CapturePhoto(string desiredName = "photo.jpg")
+        {
+            var photoStorageFile = await KnownFolders.PicturesLibrary.CreateFileAsync(desiredName, CreationCollisionOption.GenerateUniqueName);
+            await mediaCapture.CapturePhotoToStorageFileAsync(imgEncodingProperties, photoStorageFile);
+            return photoStorageFile;
+        }
+
+        public void Dispose()
+        {
+            if (mediaCapture != null)
+            {
+                mediaCapture.Dispose();
+                mediaCapture = null;
+            }
+        }
+    }
+}
---
+++ AirStream/MainPage.xaml.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using Windows.Graphics.Imaging;
+using Windows.Storage;
+using Windows.Storage.Streams;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media.Imaging;
+using Windows.UI.Xaml.Navigation;
+namespace AirStream
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private CameraCapture cameraCapture;
+        private StorageFile file;
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        /// <summary>
+        /// Invoked when this page is about to be displayed in a Frame.
+        /// </summary>
+        /// <param name="e">Event data that describes how this page was reached.
+        /// This parameter is typically used to configure the page.</param>
+        protected override async void OnNavigatedTo(NavigationEventArgs e)
+        {
+            // TODO: Prepare page for display here.
+            cameraCapture = new CameraCapture();
+            captureField.Source = await cameraCapture.Initialize();
+            await cameraCapture.StartPreview();
+            // TODO: If your application contains multiple pages, ensure that you are
+            // handling the hardware Back button by registering for the
+            // Windows.Phone.UI.Input.HardwareButtons.BackPressed event.
+            // If you are using the NavigationHelper provided by some templates,
+            // this event is handled for you.
+        }
+
+        private async void captureButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (cameraCapture != null)
+            {
+                file = await cameraCapture.CapturePhoto();
+                IRandomAccessStream imageStream = (await file.OpenStreamForReadAsync()).AsRandomAccessStream();
+                BitmapImage image = new BitmapImage();
+                image.SetSource(imageStream);
+                previewField.Source = image;
+                await cameraCapture.StopPreview();
+                captureField.Source = null;
+                cameraCapture.Dispose();
+            }
+        }
+
+        private void sendButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (file != null)
+            {
+                ShowPhoto(file.Path);
+            }
+        }
+
+        private async void ShowPhoto(String filePath)
+        {
+            Stream stream = await file.OpenStreamForReadAsync();
+            using (MemoryStream ms = new MemoryStream())
+            {
+                byte[] bStream = new byte[1024];
+                int len = -1;
+                while ((len = stream.Read(bStream, 0, 1024)) > 0)
+                {
+                    ms.Write(bStream, 0, len);
+                } 
+                ms.Position = 0;
+                ShowPhoto(ms.ToArray());
+            }
+        }
+
+        private async void ShowPhoto(byte[] photo)
+        {
+            var appleTvLink = @"http://192.168.1.118:7000/photo";
+            HttpClient client = new HttpClient();
+            client.MaxResponseContentBufferSize = 256000;
+            client.DefaultRequestHeaders.Add("User-Agent", "MediaControl/1.0");
+            client.DefaultRequestHeaders.Add("X-Apple-Session-ID", (new Guid()).ToString());
+
+            HttpRequestMessage req = new HttpRequestMessage(HttpMethod.Post, appleTvLink);
+            req.Content = new ByteArrayContent(photo);
+            var result = await client.SendAsync(req);
+        }
+    }
+}
